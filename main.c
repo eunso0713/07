2d524e923c39e7ac5ef1afcf3847aab414c2dbba
eunso0713 <aliennumber1one@sookmyung.ac.kr>
2023-10-19 15:49:18 +0900

    4 실습문제

--- main.c
+++ main.c
@@ -2,20 +2,21 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int inc(int counter);
+
+void f(void);
+
+int i;
 int main(void)
 {
-	int i=10;
-	printf("함수 호출 전 i=%d\n", i);
-	i=inc(i);
-	printf("함수 호출 후 i=%d\n", i);
-	
-	system("PAUSE");
+	for (i=0; i<5; i++)
+	{
+		f();
+	}
 	return 0;
 }
 
-int inc(int counter)
+void f(void)
 {
-	counter++;
-	return counter;
+	for (i=0; i<10; i++)
+		printf("#");
 }
