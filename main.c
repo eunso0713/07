669e0bfcf736dd724d6bfcf3a1c7218c12434784
eunso0713 <aliennumber1one@sookmyung.ac.kr>
2023-10-19 15:38:21 +0900

    3

--- main.c
+++ main.c
@@ -3,12 +3,15 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(void)
+int inc(int counter);
+int main(int argc, char *argv[])
 {
-	int i=10;
+	int i = 10;
 	printf("함수 호출 전 i=%d\n", i);
 	inc(i);
 	printf("함수 호출 후 i=%d\n", i);
+	
+	system("PAUSE");
 	return 0;
 }
 
